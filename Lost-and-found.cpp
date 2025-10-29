203e48e60bbbd3d783da395949a144c7d5ee87a8
Nycx04 <johndavecordoto04@gmail.com>
2025-10-29 08:56:11 +0800

    Update Lost-and-found.cpp

--- Lost-and-found.cpp
+++ Lost-and-found.cpp
@@ -5,8 +5,11 @@
 //adjust code later for linked list coz i dont fully understand what he means by that
 //need to be able to tell how many lost item in a category (can be done with for sizeof)
 // still arguing with myself to use switch for categories
+// decided to use switch for category to make it intercative where user will choose which category they want to view
 
 int main() {
+    
+    int choices;
     
     std::string categories[] = {"Clothes", "Toys", "Electronic devices"};
     int size = sizeof(categories) / sizeof(categories[0]);
@@ -17,14 +20,27 @@ int main() {
     
     std::string electronics[] = {"Cellphone", "laptop", "tablet"};
     
-    //shpws categories
-    std::cout << "Categories: \n";
-    for(int i = 0; i < size; i++) {
-    std::cout << categories[i] << "  \n";
-    }
+    std::cout << "Choose Category nuumber: ";
+    std::cin >> choices;
 
+    switch (choices)
+    {
+    case 1: {//for clothes 
+        int clothesize = sizeof(Clothes) / sizeof(Clothes[0]);
+        for(int i = 0; i < clothesize; i++) {
+        std::cout << Clothes[i];
+        }
+        break;
+    }
+    case 2: {
+        int toysize = sizeof(toysize) / sizeof(toysize[0]);
+        for(int i = 0; i < toysize; i++) {
+            
+        }   //gym time update later
+        break;
+    }
     
-    
+}
     
     
     
